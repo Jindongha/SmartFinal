b33a12df19134c97d0b8dd998ffdb7313aeae5cb
lonelyguy17 <lonelyguy142@gmail.com>
2014-11-06 21:23:00 +0900

    test

--- src/org/androidtown/ui/linearlayout/Teacher_Account_Activity.java
+++ src/org/androidtown/ui/linearlayout/Teacher_Account_Activity.java
@@ -8,7 +8,7 @@ public class Teacher_Account_Activity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        //test
         setContentView(R.layout.teacher_account_layout);
 	}
 
